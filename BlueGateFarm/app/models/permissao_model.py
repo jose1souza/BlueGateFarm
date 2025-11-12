47e22dd910ec2ad9bf6ab39413aedb1fa392a848
Nicolas Alves <nicolasbrodith@gmail.com>
2025-11-12 19:32:15 -0300

    Add files via upload

---
+++ BlueGateFarm/app/models/permissao_model.py
@@ -0,0 +1,16 @@
+from app import db 
+
+class permissao(db.Model):
+    __tablename__ = 'permissao'
+
+    id_permissao = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), unique=True, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id_permissao': self.id_permissao,
+            'nome': self.nome,
+        }
+        
+    def __repr__(self):
+        return f'<Permissao {self.nome}>'
--- BlueGateFarm/run.py
+++ BlueGateFarm/run.py
@@ -1,8 +1,6 @@
 from app import create_app
 
-# Cria a instância da aplicação
 app = create_app()
 
 if __name__ == '__main__':
-    # Roda a aplicação no modo de desenvolvimento
     app.run(debug=True)
